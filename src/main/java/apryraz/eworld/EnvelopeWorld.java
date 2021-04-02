7557eedc404bfa6425139448e7584dba22fb8ecf
santo0 <30506769+santo0@users.noreply.github.com>
2021-04-02 13:39:18 +0200

    First steps

--- src/main/java/apryraz/eworld/EnvelopeWorld.java
+++ src/main/java/apryraz/eworld/EnvelopeWorld.java
@@ -1,64 +1,109 @@
 package apryraz.eworld;
 
 
-
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+
 import org.sat4j.specs.*;
 import org.sat4j.minisat.*;
 import org.sat4j.reader.*;
 
 
 /**
-  The class for the main program of the Barcenas World
-
-**/
+ * The class for the main program of the Barcenas World
+ **/
 public class EnvelopeWorld {
 
 
+    /**
+     * This function should execute the sequence of steps stored in the file fileSteps,
+     * but only up to numSteps steps. Each step must be executed with function
+     * runNextStep() of the BarcenasFinder agent.
+     *
+     * @param wDim          the dimension of world
+     * @param numSteps      num of steps to perform
+     * @param fileSteps     file name with sequence of steps to perform
+     * @param fileEnvelopes file name with sequence of steps to perform
+     **/
+    public static void runStepsSequence(int wDim,
+                                        int numSteps, String fileSteps, String fileEnvelopes) throws
+            IOException, ContradictionException, TimeoutException {
+        // Make instances of TreasureFinder agent and environment object classes
+        EnvelopeFinder EAgent;
+        EnvelopeWorldEnv EnvAgent;
 
-/**
-   This function should execute the sequence of steps stored in the file fileSteps,
-   but only up to numSteps steps. Each step must be executed with function 
-   runNextStep() of the BarcenasFinder agent.
+        EAgent = new EnvelopeFinder(wDim);
+        EnvAgent = new EnvelopeWorldEnv(wDim, fileEnvelopes);
 
-   @param wDim the dimension of world
-   @param numSteps num of steps to perform
-   @param fileSteps file name with sequence of steps to perform
-   @param fileEnvelopes file name with sequence of steps to perform
+        // save environment object into EAgent
+        EAgent.setEnvironment(EnvAgent);
 
-**/
-public static void runStepsSequence( int wDim, 
-                                    int numSteps, String fileSteps, String fileEnvelopes ) throws
-                               IOException,  ContradictionException, TimeoutException {
-  // Make instances of TreasureFinder agent and environment object classes
-   EnvelopeFinder EAgent  ;
-   EnvelopeWorldEnv EnvAgent  ;  
 
-  
+        // load list of steps into the Finder Agent
+        EAgent.loadListOfSteps(numSteps, fileSteps);
 
-   // save environment object into EAgent 
- 
 
-   // load list of steps into the Finder Agent
-    
-    
-   // Execute sequence of steps with the Agent
+        // Execute sequence of steps with the Agent
+        for (int i = 0; i < numSteps; i++) {
+            EAgent.runNextStep();
+        }
+    }
 
-}
+    /**
+     * This function should load five arguments from the command line:
+     * arg[0] = dimension of the word
+     * arg[3] = num of steps to perform
+     * arg[4] = file name with sequence of steps to perform
+     * arg[5] = file name with list of envelopes positions
+     **/
+    public static void main(String[] args) throws ParseFormatException,
+            IOException, ContradictionException, TimeoutException, IllegalArgumentException {
 
-/**
-*  This function should load five arguments from the command line:
-*  arg[0] = dimension of the word
-*  arg[3] = num of steps to perform
-*  arg[4] = file name with sequence of steps to perform
-*  arg[5] = file name with list of envelopes positions
-**/
-public static void main ( String[] args) throws ParseFormatException,
-        IOException,  ContradictionException, TimeoutException {
+        if (args.length != 4) {
+            System.out.println("Usage: EnvelopeWorld wdim numSteps fileSteps fileEnvelopes");
+            throw new IllegalArgumentException(String.format("Expected 4 arguments. Given %d", args.length));
+        }
+        int wDim = getWDim(args[0]);
+        int numSteps = getNumSteps(args[1]);
+        String fileSteps;
+        if (new File(args[2]).exists()) {
+            fileSteps = args[2];
+        } else {
+            System.out.printf("Path %s doesn't exist%n", args[2]);
+            throw new IllegalArgumentException();
+        }
+        String fileEnvelopes;
+        if (new File(args[2]).exists()) {
+            fileEnvelopes = args[3];
+        } else {
+            System.out.printf("Path %s doesn't exist%n", args[3]);
+            throw new IllegalArgumentException();
+        }
 
-  // Here I run a concrete example, but you should read parameters from
-  // the command line, as decribed above.
-  runStepsSequence(  4, 5, "tests/steps1.txt", "tests/envelopes1.txt"  );
-}
+        // Here I run a concrete example, but you should read parameters from
+        // the command line, as decribed above.
+        runStepsSequence(wDim, numSteps, fileSteps, fileEnvelopes);
+    }
 
+    private static int getWDim(String arg) throws IllegalArgumentException {
+        int wDim;
+        try {
+            wDim = Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return wDim;
+    }
+
+    private static int getNumSteps(String arg) throws IllegalArgumentException {
+        int numSteps;
+        try {
+            numSteps = Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return numSteps;
+    }
 }
--- src/main/java/apryraz/eworld/EnvelopeWorldEnv.java
+++ src/main/java/apryraz/eworld/EnvelopeWorldEnv.java
@@ -2,93 +2,122 @@
 
 package apryraz.eworld;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-
-
+import static java.lang.System.exit;
 
 
 public class EnvelopeWorldEnv {
-/**
-   world dimension
+    /**
+     * world dimension
+     **/
+    int WorldDim;
 
-**/
-  int  WorldDim;
+    /**
+     * envelopes locations
+     */
+    Set<Position> envelopesLocations;   //Todo: Hauria d'implementar hash a Position?
 
+    /**
+     * number of envelopes
+     */
+    int numEnvelopes;
 
-/**
-*  Class constructor
-*
-* @param dim dimension of the world
-* @param envelopeFile File with list of envelopes locations
-**/
-  public EnvelopeWorldEnv( int dim, String envelopeFile ) {
+    /**
+     * Class constructor
+     *
+     * @param dim          dimension of the world
+     * @param envelopeFile File with list of envelopes locations
+     **/
+    public EnvelopeWorldEnv(int dim, String envelopeFile) {
 
-    WorldDim = dim;
-    loadEnvelopeLocations(envelopeFile);
-  }
+        WorldDim = dim;
+        loadEnvelopeLocations(envelopeFile);
+    }
 
-/**
-*   Load the list of pirates locations
-*  
-*    @param: name of the file that should contain a
-*            set of envelope locations in a single line.
-**/
-  public void loadEnvelopeLocations( String envelopeFile ) {
-
-
-  }
-
-
-/**
-* Process a message received by the EFinder agent,
-* by returning an appropriate answer
-* It should answer to moveto and detectsat messages
-*
-* @param   msg message sent by the Agent
-*
-* @return  a msg with the answer to return to the agent
-**/
-   public AMessage acceptMessage( AMessage msg ) {
-       AMessage ans = new AMessage("voidmsg", "", "", "" );
-
-         msg.showMessage();
-       if ( msg.getComp(0).equals("moveto") ) {
-           int nx = Integer.parseInt( msg.getComp(1) );
-           int ny = Integer.parseInt( msg.getComp(2) );
-           
-           if (withinLimits(nx,ny))
-           {
-             
-             
-             ans = new AMessage("movedto",msg.getComp(1),msg.getComp(2)  );
-           }
-           else
-             ans = new AMessage("notmovedto",msg.getComp(1),msg.getComp(2), "" );
-
-       } else {
-             // YOU MUST ANSWER HERE TO THE OTHER MESSAGE TYPE:
-             //   ( "detectsat", "x" , "y", "" )
-             //
-         }
-       return ans;
-
-   }
+    /**
+     * Load the list of pirates locations
+     *
+     * @param: name of the file that should contain a
+     * set of envelope locations in a single line.
+     **/
+    public void loadEnvelopeLocations(String envelopeFile) {
+        String[] positionList;
+        String locations = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(envelopeFile));
+            System.out.println("ENVELOPE LOCATIONS FILE OPENED ...");
+            locations = br.readLine();
+            br.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("MSG.   => Envelope locations file not found");
+            exit(1);
+        } catch (IOException ex) {
+            Logger.getLogger(EnvelopeWorldEnv.class.getName()).log(Level.SEVERE, null, ex);
+            exit(2);
+        }
+        positionList = locations.split(" ");
+        envelopesLocations = new LinkedHashSet<>();
+        for(String location: positionList){
+            String[] coords = location.split(",");
+            envelopesLocations.add(new Position(Integer.parseInt(coords[0]), Integer.parseInt(coords[1])));
+        }
+        numEnvelopes = envelopesLocations.size();
+    }
 
 
+    /**
+     * Process a message received by the EFinder agent,
+     * by returning an appropriate answer
+     * It should answer to moveto and detectsat messages
+     *
+     * @param msg message sent by the Agent
+     * @return a msg with the answer to return to the agent
+     **/
+    public AMessage acceptMessage(AMessage msg) {
+        AMessage ans = new AMessage("voidmsg", "", "", "");
 
- /**
-  * Check if position x,y is within the limits of the
-  * WorldDim x WorldDim   world
-  *
-  * @param x  x coordinate of agent position
-  * @param y  y coordinate of agent position
-  *
-  * @return true if (x,y) is within the limits of the world
-  **/
-   public boolean withinLimits( int x, int y ) {
+        msg.showMessage();
+        if (msg.getComp(0).equals("moveto")) {
+            int nx = Integer.parseInt(msg.getComp(1));
+            int ny = Integer.parseInt(msg.getComp(2));
 
-    return ( x >= 1 && x <= WorldDim && y >= 1 && y <= WorldDim);
-  }
- 
+            if (withinLimits(nx, ny)) {
+
+
+                ans = new AMessage("movedto", msg.getComp(1), msg.getComp(2), "");
+            } else
+                ans = new AMessage("notmovedto", msg.getComp(1), msg.getComp(2), "");
+
+        } else {
+            // YOU MUST ANSWER HERE TO THE OTHER MESSAGE TYPE:
+            //   ( "detectsat", "x" , "y", "" )
+            //
+        }
+        return ans;
+
+    }
+
+
+    /**
+     * Check if position x,y is within the limits of the
+     * WorldDim x WorldDim   world
+     *
+     * @param x x coordinate of agent position
+     * @param y y coordinate of agent position
+     * @return true if (x,y) is within the limits of the world
+     **/
+    public boolean withinLimits(int x, int y) {
+
+        return (x >= 1 && x <= WorldDim && y >= 1 && y <= WorldDim);
+    }
+
 }
